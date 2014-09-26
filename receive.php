d6a11946188287e79a85a984ef1f52ccee6e6961
Daniel <dusoft@dusoft>
2014-09-26 13:31:51 +0200

    future web UI additions bug fixes connectors created eurosms connector separated

--- receive.php
+++ receive.php
@@ -1,22 +1,21 @@
 <?php
-require('functions.php');
+require("config.php");
+require("db.class.php");
+require("actions-sms.php");
+require("connectors/".$connectors["sms"]);
 
 $db=new Database($dbServer,$dbUser,$dbPassword,$dbName);
 $db->connect();
 
-$message = strtoupper(trim(urldecode($_GET["sms_text"])));
-$number = $_GET["sender"]; //$number = intval($_GET["sender"]);
-$sms_uuid = $_GET["sms_uuid"];
+$sms=new SMSConnector();
 
-log_sms($sms_uuid,$number,$_GET["receive_time"],$_GET["sms_text"],$_SERVER['REMOTE_ADDR']);
+log_sms($sms->UUID(),$sms->Number(),$sms->Time(),$sms->Text(),$sms->IPAddress());
 
-if (DEBUG!==TRUE) echo 'ok:',$sms_uuid,"\n"; // confirm SMS received to API
+$args = explode(" ", $sms->ProcessedText());
 
-$args = preg_split("/\s+/", $message);
-
-if(!validateNumber($number))
+if(!validateNumber($sms->Number()))
    {
-   sendSMS($number,"Vase cislo nie je registrovane. Your number is not registered.");
+   sendSMS($sms->Number(),"Vase cislo nie je registrovane. Your number is not registered.");
    }
 else
    {
@@ -24,63 +23,66 @@ else
       {
       case "HELP":
       case "POMOC":
-         help($number);
+         help($sms->Number());
          break;
       case "FREE":
-         freeBikes($number);
+         freeBikes($sms->Number());
          break;
       case "RENT":
       case "POZICAJ":
-         validateReceivedSMS($number,count($args),2,"with bike number: RENT 47");
-         rent($number,$args[1]);//intval
+         validateReceivedSMS($sms->Number(),count($args),2,"with bike number: RENT 47");
+         rent($sms->Number(),$args[1]);//intval
          break;
       case "RETURN":
       case "VRAT":
-         validateReceivedSMS($number,count($args),3,"with bike number and stand name: RETURN 47 RACKO");
-         returnBike($number,$args[1],$args[2]);
+         validateReceivedSMS($sms->Number(),count($args),3,"with bike number and stand name: RETURN 47 RACKO");
+         returnBike($sms->Number(),$args[1],$args[2]);
          break;
       case "WHERE":
+      case "WHO":
       case "KDE":
-         validateReceivedSMS($number,count($args),2,"with bike number: WHERE 47");
-         where($number,$args[1]);
+      case "KTO":
+         validateReceivedSMS($sms->Number(),count($args),2,"with bike number: WHERE 47");
+         where($sms->Number(),$args[1]);
          break;
       case "INFO":
-         validateReceivedSMS($number,count($args),2,"with stand name: INFO RACKO");
-         info($number,$args[1]);
+         validateReceivedSMS($sms->Number(),count($args),2,"with stand name: INFO RACKO");
+         info($sms->Number(),$args[1]);
          break;
       case "NOTE":
-         validateReceivedSMS($number,count($args),2,"with bike number and problem description: NOTE 47 Flat tire on front wheel");
-         note($number,$args[1],trim(urldecode($_GET["sms_text"])));
+         validateReceivedSMS($sms->Number(),count($args),2,"with bike number and problem description: NOTE 47 Flat tire on front wheel");
+         note($sms->Number(),$args[1],trim(urldecode($sms->Text())));
          break;
       case "LIST":
       case "ZOZNAM":
-         checkUserPrivileges($number);
-         validateReceivedSMS($number,count($args),2,"with stand name: LIST RACKO");
-         listBikes($number,$args[1]);
+         checkUserPrivileges($sms->Number());
+         validateReceivedSMS($sms->Number(),count($args),2,"with stand name: LIST RACKO");
+         listBikes($sms->Number(),$args[1]);
          break;
       case "ADD":
-         checkUserPrivileges($number);
-         validateReceivedSMS($number,count($args),3,"with email, phone, fullname: ADD king@earth.com 0901456789 Martin Luther King Jr.");
-         add($number,$args[1],$args[2],trim(urldecode($_GET["sms_text"])));
+         checkUserPrivileges($sms->Number());
+         validateReceivedSMS($sms->Number(),count($args),3,"with email, phone, fullname: ADD king@earth.com 0901456789 Martin Luther King Jr.");
+         add($sms->Number(),$args[1],$args[2],trim(urldecode($sms->Text())));
          break;
       case "REVERT":
-         checkUserPrivileges($number);
-         validateReceivedSMS($number,count($args),1,"with bike number: REVERT 47");
-         revert($number,$args[1]);
+         checkUserPrivileges($sms->Number());
+         validateReceivedSMS($sms->Number(),count($args),2,"with bike number: REVERT 47");
+         revert($sms->Number(),$args[1]);
          break;
       //    case "NEAR":
       //    case "BLIZKO":
-      //	near($number,$args[1]);
+      //	near($sms->Number(),$args[1]);
       case "LAST":
-         checkUserPrivileges($number);
-         validateReceivedSMS($number,count($args),2,"with bike number: LAST 47");
-         last($number,$args[1]);
+         checkUserPrivileges($sms->Number());
+         validateReceivedSMS($sms->Number(),count($args),2,"with bike number: LAST 47");
+         last($sms->Number(),$args[1]);
          break;
       default:
-         unknownCommand($number,$args[0]);
+         unknownCommand($sms->Number(),$args[0]);
       }
    }
 
 $db->conn->commit();
+$sms->Respond();
 
 ?>
