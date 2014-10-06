e5cbdb1c3c93c7235094d697605b243fcdb8bf7e
Daniel <dusoft@dusoft>
2014-10-06 13:54:39 +0200

    not null removed

--- changes.sql
+++ changes.sql
@@ -2,8 +2,8 @@
 ALTER TABLE `stands`
 ADD `standPhoto` varchar(255) COLLATE 'utf8_general_ci' AFTER `standDescription`,
 COMMENT='';
-CHANGE `standDescription` `standDescription` varchar(100) COLLATE 'utf8_general_ci' NOT NULL AFTER `standName`,
-CHANGE `standPhoto` `standPhoto` varchar(255) COLLATE 'utf8_general_ci' NOT NULL AFTER `standDescription`,
+CHANGE `standDescription` `standDescription` varchar(100) COLLATE 'utf8_general_ci' AFTER `standName`,
+CHANGE `standPhoto` `standPhoto` varchar(255) COLLATE 'utf8_general_ci' AFTER `standDescription`,
 COMMENT='';
 UPDATE stands set standPhoto='' where standPhoto=NULL
 
