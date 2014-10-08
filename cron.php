44e6c3fb3bed4e11af2e485f0297adbe03cdf3bf
Daniel <dusoft@dusoft>
2014-10-09 01:40:53 +0200

    cron for checks and balances to be run every hour (or more often)

---
+++ cron.php
@@ -0,0 +1,10 @@
+<?php
+require("config.php");
+require("db.class.php");
+require("actions-web.php");
+
+$db=new Database($dbServer,$dbUser,$dbPassword,$dbName);
+$db->connect();
+
+checklongrental();
+?>
