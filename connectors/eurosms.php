06fda313a8a1c13fdfb8a5f283b768ea8494687e
Daniel <dusoft@dusoft>
2014-10-09 18:51:08 +0200

    config for connector moved into the connect itself. bug fixes

--- connectors/eurosms.php
+++ connectors/eurosms.php
@@ -1,15 +1,27 @@
 <?php
 
+$gatewayId="";
+$gatewayKey="";
+$gatewaySenderNumber="";
+
 class SMSConnector
    {
 
    function __construct($connector)
       {
-      $this->message=$_GET["sms_text"];
-      $this->number=$_GET["sender"];
-      $this->uuid=$_GET["sms_uuid"];
-      $this->time=$_GET["receive_time"];
+      $this->CheckConfig();
+      if (isset($_GET["sms_text"])) $this->message=$_GET["sms_text"];
+      if (isset($_GET["sender"])) $this->number=$_GET["sender"];
+      if (isset($_GET["sms_uuid"])) $this->uuid=$_GET["sms_uuid"];
+      if (isset($_GET["receive_time"])) $this->time=$_GET["receive_time"];
       $this->ipaddress=$_SERVER['REMOTE_ADDR'];
+      }
+
+   function CheckConfig()
+      {
+      global $gatewayId,$gatewayKey,$gatewaySenderNumber;
+      if (DEBUG===TRUE) return;
+      if (!$gatewayId OR !$gatewayKey OR !$gatewaySenderNumber) exit('Please, configure SMS API gateway access in '.__FILE__.'!');
       }
 
    function Text()
@@ -49,9 +61,11 @@ class SMSConnector
       echo 'ok:',$this->uuid,"\n";
       }
 
+   // send SMS message via API
    function Send($number,$text)
       {
       global $gatewayId,$gatewayKey,$gatewaySenderNumber;
+      if (DEBUG===TRUE) return;
       $s=substr(md5($gatewayKey.$number),10,11);
       $um=urlencode($text);
       fopen("http://as.eurosms.com/sms/Sender?action=send1SMSHTTP&i=$gatewayId&s=$s&d=1&sender=$gatewaySenderNumber&number=$number&msg=$um","r");
