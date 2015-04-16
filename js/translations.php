3a08dd8805c2539bef2a5174e19e0c0df3749eab
Daniel <dusoft@staznosti.sk>
2015-04-16 23:57:20 +0200

    translations

---
+++ js/translations.php
@@ -0,0 +1,26 @@
+_select_stand = "<?php echo _('Select stand'); ?>";
+_bicycle = "<?php echo _('bicycle'); ?>";
+_bicycles = "<?php echo _('bicycles'); ?>";
+_no_bicycles = "<?php echo _('No bicycles'); ?>";
+_open_map = "<?php echo _('Open a map with directions to the selected stand from your current location.'); ?>";
+_walking_directions = "<?php echo _('walking directions'); ?>";
+_display_photo = "<?php echo _('Display photo of the stand.'); ?>";
+_photo = "<?php echo _('photo'); ?>";
+_currently_rented = "<?php echo _('You have this bicycle currently rented. The current lock code is displayed below the bike number.'); ?>";
+_reported_problem = "<?php echo _('Reported problem on this bicycle:'); ?>";
+
+_sms_code = "<?php echo _('SMS code is in AB 123456 format.'); ?>";
+_enter_sms_code = "<?php echo _('Please, enter SMS code received to your phone.'); ?>";
+_enter_names = "<?php echo _('Please, enter your firstname and lastname.'); ?>";
+_email_incorrect = "<?php echo _('Email address is incorrect.'); ?>";
+_enter_email = "<?php echo _('Please, enter your email.'); ?>";
+_passwords_nomatch = "<?php echo _('Passwords do not match.'); ?>";
+_enter_password = "<?php echo _('Please, enter your password.'); ?>";
+_existing_user = "<?php echo _('Existing user password change'); ?>";
+_step2 = "<?php echo _('Step 2 - Change password'); ?>";
+_set_password = "<?php echo _('Set password'); ?>";
+
+_user = "<?php echo _('User'); ?>";
+_privileges = "<?php echo _('Privileges'); ?>";
+_limit = "<?php echo _('Limit'); ?>";
+_credit = "<?php echo _('Credit'); ?>";
