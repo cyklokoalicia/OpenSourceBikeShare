6982740b2e0158d5da7ff3abd7838931505ec472
Daniel Duris <dusoft@gmail.com>
2016-08-15 14:57:20 +0200

    db column names all to lowercase

--- create-database.sql
+++ create-database.sql
@@ -6,22 +6,22 @@ SET time_zone = '+00:00';
 DROP TABLE IF EXISTS `bikes`;
 CREATE TABLE `bikes` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `bikeNum` int(11) NOT NULL,
+  `bikenum` int(11) NOT NULL,
   `currentUser` int(11) DEFAULT NULL,
   `currentStand` int(11) DEFAULT NULL,
   `currentCode` int(11) NOT NULL,
   `note` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY (`bikeNum`)
+  UNIQUE KEY (`bikenum`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 DROP TABLE IF EXISTS `credit`;
 CREATE TABLE `credit` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `userId` int(11) NOT NULL,
+  `userid` int(11) NOT NULL,
   `credit` float(5,2) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY (`userId`)
+  UNIQUE KEY (`userid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 DROP TABLE IF EXISTS `coupons`;
@@ -37,8 +37,8 @@ CREATE TABLE `coupons` (
 DROP TABLE IF EXISTS `notes`;
 CREATE TABLE `notes` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `bikeNum` int(11) NOT NULL,
-  `userId` int(11) NOT NULL,
+  `bikenum` int(11) NOT NULL,
+  `userid` int(11) NOT NULL,
   `note` varchar(100),
   `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `deleted` timestamp NULL,
@@ -49,7 +49,7 @@ CREATE TABLE `notes` (
 DROP TABLE IF EXISTS `geolocation`;
 CREATE TABLE `geolocation` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `userId` int(10) unsigned NOT NULL,
+  `userid` int(10) unsigned NOT NULL,
   `longitude` double(20,17) NOT NULL,
   `latitude` double(20,17) NOT NULL,
   `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -60,13 +60,13 @@ CREATE TABLE `geolocation` (
 DROP TABLE IF EXISTS `history`;
 CREATE TABLE `history` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `userId` int(11) NOT NULL,
-  `bikeNum` int(11) NOT NULL,
+  `userid` int(11) NOT NULL,
+  `bikenum` int(11) NOT NULL,
   `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `action` varchar(20) NOT NULL,
   `parameter` text NOT NULL,
-  `standId` int(11) DEFAULT NULL,
-  `pairAction` timestamp NULL DEFAULT NULL,
+  `standid` int(11) DEFAULT NULL,
+  `pairaction` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -74,10 +74,10 @@ CREATE TABLE `history` (
 DROP TABLE IF EXISTS `limits`;
 CREATE TABLE `limits` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `userId` int(11) NOT NULL,
-  `userLimit` int(11) NOT NULL DEFAULT '1',
+  `userid` int(11) NOT NULL,
+  `userlimit` int(11) NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`),
-  UNIQUE KEY (`userId`)
+  UNIQUE KEY (`userid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
@@ -98,7 +98,7 @@ CREATE TABLE `received` (
   `sender` varchar(20) NOT NULL,
   `receivetime` varchar(20) NOT NULL,
   `smstext` varchar(200) NOT NULL,
-  `IP` varchar(20) NOT NULL,
+  `ip` varchar(20) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -106,8 +106,8 @@ CREATE TABLE `received` (
 DROP TABLE IF EXISTS `registration`;
 CREATE TABLE `registration` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `userId` int(11) NOT NULL,
-  `userKey` varchar(64) NOT NULL,
+  `userid` int(11) NOT NULL,
+  `userkey` varchar(64) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -125,34 +125,34 @@ CREATE TABLE `sent` (
 DROP TABLE IF EXISTS `sessions`;
 CREATE TABLE `sessions` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `userId` int(10) unsigned NOT NULL,
-  `sessionId` varchar(256) CHARACTER SET latin1 NOT NULL,
-  `timeStamp` varchar(256) CHARACTER SET latin1 NOT NULL,
+  `userid` int(10) unsigned NOT NULL,
+  `sessionid` varchar(256) CHARACTER SET latin1 NOT NULL,
+  `timestamp` varchar(256) CHARACTER SET latin1 NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `userId` (`userId`),
-  KEY `sessionId` (`sessionId`)
+  UNIQUE KEY `userid` (`userid`),
+  KEY `sessionid` (`sessionid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
 DROP TABLE IF EXISTS `stands`;
 CREATE TABLE `stands` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `standName` varchar(50) NOT NULL,
-  `standDescription` varchar(255),
-  `standPhoto` varchar(255),
-  `serviceTag` int(10),
-  `placeName` varchar(50) NOT NULL,
+  `standname` varchar(50) NOT NULL,
+  `standdescription` varchar(255),
+  `standphoto` varchar(255),
+  `servicetag` int(10),
+  `placename` varchar(50) NOT NULL,
   `longitude` double(20,17),
   `latitude` double(20,17),
   PRIMARY KEY (`id`),
-  UNIQUE KEY `standName` (`standName`)
+  UNIQUE KEY `standname` (`standname`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `userName` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
   `password` text NOT NULL,
   `mail` varchar(255) NOT NULL,
   `number` varchar(30),
--- db-rb-setup.php
+++ db-rb-setup.php
@@ -4,10 +4,8 @@ require("external/rb.php");
 
 R::setup('mysql:host='.$dbserver.';dbname='.$dbname, $dbuser, $dbpassword);
 R::freeze(true);
-R::debug(true, 1); //select MODE 2 to see parameters filled in
-// R::fancyDebug();   //since 4.2
+//R::fancyDebug();
 
-// R::addDatabase('localdb', 'mysql:host='.$dbserver.';dbname='.$dbname, $dbuser, $dbpassword, true);
-R::begin();
+R::addDatabase('localdb', 'mysql:host='.$dbserver.';dbname='.$dbname, $dbuser, $dbpassword, true);
 
 ?>
