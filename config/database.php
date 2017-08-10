6ca9faec48e4b46769ffd9604fc012bcc548d4b9
mirosvitok@gmail.com <mirosvitok@gmail.com>
2017-08-10 17:37:23 +0200

    Configuration for DB testing

--- config/database.php
+++ config/database.php
@@ -54,6 +54,21 @@ return [
             'engine' => null,
         ],
 
+        'mysql_testing' => [
+            'driver' => 'mysql',
+            'host' => env('DB_HOST_TEST', 'localhost'),
+            'port' => env('DB_PORT_TEST', '3306'),
+            'database' => env('DB_DATABASE_TEST', 'forge'),
+            'username' => env('DB_USERNAME_TEST', 'forge'),
+            'password' => env('DB_PASSWORD_TEST_', ''),
+            'unix_socket' => env('DB_SOCKET_TEST', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'strict' => true,
+            'engine' => null,
+        ],
+
         'pgsql' => [
             'driver' => 'pgsql',
             'host' => env('DB_HOST', 'localhost'),
