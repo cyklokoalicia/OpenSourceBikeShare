e729333add3f19001284254c56835d81d8ead5c9
Miroslav Svítok <mirosvitok@gmail.com>
2018-04-02 10:18:58 +0200

    Fix: replace json with text to support MariaDB

--- database/migrations/2017_08_14_193116_create_media_table.php
+++ database/migrations/2017_08_14_193116_create_media_table.php
@@ -20,8 +20,8 @@ class CreateMediaTable extends Migration
             $table->string('mime_type')->nullable();
             $table->string('disk');
             $table->unsignedInteger('size');
-            $table->json('manipulations');
-            $table->json('custom_properties');
+            $table->text('manipulations');
+            $table->text('custom_properties');
             $table->unsignedInteger('order_column')->nullable();
             $table->nullableTimestamps();
         });
