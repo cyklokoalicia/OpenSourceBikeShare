994de87708e831b79cd51c764e42c8fe7916671d
palo <pavol@sivy.net>
2019-09-14 19:38:04 +0200

    Removed protocol from openstreet url for correct https loading

--- js/functions.js
+++ js/functions.js
@@ -37,7 +37,7 @@ function mapinit()
    map = new L.Map('map');
 
    // create the tile layer with correct attribution
-   var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+   var osmUrl='//{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
    var osmAttrib='Map data (c) <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
    var osm = new L.TileLayer(osmUrl, {minZoom: 8, maxZoom: 19, attribution: osmAttrib});
 
