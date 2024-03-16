d3797744e8e5bd7df940def8524d4a11a87386ae
Sveneld <sveneld300@gmail.com>
2024-03-16 23:55:00 +0100

    Fix credit

--- src/Credit/CreditSystem.php
+++ src/Credit/CreditSystem.php
@@ -39,7 +39,7 @@ class CreditSystem implements CreditSystemInterface
 
     public function getUserCredit($userid)
     {
-        $result = $this->db->query("SELECT credit FROM users WHERE id = '$userid'");
+        $result = $this->db->query("SELECT credit FROM credit WHERE id = '$userid'");
         if ($result->rowCount() == 0) {
             return 0;
         }
