da542d26d03f22ae2eb409f3928db42cdb8302d7
Sveneld <sveneld300@gmail.com>
2024-04-03 19:55:20 +0200

    increase code on qr rent page

--- src/Rent/RentSystemQR.php
+++ src/Rent/RentSystemQR.php
@@ -92,7 +92,7 @@ class RentSystemQR extends AbstractRentSystem implements RentSystemInterface
             }
         }
 
-        $message = '<h3>' . _('Bike') . ' ' . $bikeNum . ': <span class="label label-primary">' . _('Open with code') . ' ' . $currentCode . '.</span></h3>' . _('Change code immediately to') . ' <span class="label label-default" style="font-size: 16px;">' . $newCode . '</span><br />' . _('(open, rotate metal part, set new code, rotate metal part back)') . '.';
+        $message = '<h3>' . _('Bike') . ' ' . $bikeNum . ': <span class="label label-primary">' . _('Open with code') . ' ' . $currentCode . '.</span></h3><h3>' . _('Change code immediately to') . ' <span class="label label-default" style="font-size: 16px;">' . $newCode . '</span></h3>' . _('(open, rotate metal part, set new code, rotate metal part back)') . '.';
         if ($note) {
             $message .= '<br />' . _('Reported issue') . ': <em>' . $note . '</em>';
         }
