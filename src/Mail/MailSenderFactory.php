11942c3e507433ed2c658d268c5cdce770777ed6
Sveneld <sveneld300@gmail.com>
2025-01-11 14:09:45 +0100

    add phpmailer debug logger

--- src/Mail/MailSenderFactory.php
+++ src/Mail/MailSenderFactory.php
@@ -8,17 +8,20 @@ use Symfony\Component\DependencyInjection\ServiceLocator;
 
 class MailSenderFactory
 {
+    private string $smtpHost;
     private ServiceLocator $locator;
 
     public function __construct(
+        string $smtpHost,
         ServiceLocator $locator
     ) {
+        $this->smtpHost = $smtpHost;
         $this->locator = $locator;
     }
 
     public function getMailSender(): MailSenderInterface
     {
-        if (DEBUG === true) {
+        if (empty($this->smtpHost)) {
             return $this->locator->get(DebugMailSender::class);
         } else {
             return $this->locator->get(PHPMailerMailSender::class);
