9a52a955246aaa6f5eb50c9dc96f9d3d664a3306
Sveneld <sveneld300@gmail.com>
2025-02-15 14:30:44 +0100

    show yearly stats only at the end of year

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -22,16 +22,16 @@ class HomeController extends AbstractController
     ): Response {
 
         //show stats for current year if it is end of the year
-        $currentData = new \DateTimeImmutable();
-        if ($currentData->format('z') > 350) {
+        $currentDate = new \DateTimeImmutable();
+        if ($currentDate->format('z') > 350) {
             $personalStatsYearUrl = $this->generateUrl(
                 'personal_stats_year',
-                ['year' => $currentData->format('Y')]
+                ['year' => $currentDate->format('Y')]
             );
-        } elseif ($currentData->format('z') < 30) {
+        } elseif ($currentDate->format('z') < 30) {
             $personalStatsYearUrl = $this->generateUrl(
                 'personal_stats_year',
-                ['year' => $currentData->format('Y') - 1]
+                ['year' => $currentDate->format('Y') - 1]
             );
         } else {
             $personalStatsYearUrl = null;
