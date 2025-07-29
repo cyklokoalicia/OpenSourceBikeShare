d49525152ee2194ca75aadc101b5c70e5ce975d7
Sveneld <sveneld300@gmail.com>
2025-07-29 21:46:02 +0200

    psr

--- src/App/Entity/User.php
+++ src/App/Entity/User.php
@@ -85,7 +85,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         return null;
     }
 
-    public function eraseCredentials()
+    public function eraseCredentials(): void
     {
         // If you store any temporary, sensitive data on the user, clear it here
     }
