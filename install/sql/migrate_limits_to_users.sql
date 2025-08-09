c7e4a65d9a458992ddc9009e307e3cd108d98be8
Sveneld <sveneld300@gmail.com>
2025-08-09 19:48:08 +0200

    Inline user limits

--- install/sql/migrate_limits_to_users.sql
+++
@@ -1,5 +0,0 @@
--- Migrate user limits from separate `limits` table to `users` table
-ALTER TABLE users ADD COLUMN userLimit INT(11) UNSIGNED NOT NULL DEFAULT 0;
-UPDATE users u JOIN limits l ON u.userId = l.userId SET u.userLimit = l.userLimit;
-DROP TABLE limits;
-
